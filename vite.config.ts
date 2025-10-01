007408728465d9d5a1d94b9bb33f1582172e7ae1
Muhammed Shabeer OP <androlite4@gmail.com>
2025-10-01 14:39:01 +0530

    feat: Add sitemap generation functionality and update build script

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import Sitemap from 'vite-plugin-sitemap'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), Sitemap({ hostname: 'https://edhop.in' }),
+  ],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
